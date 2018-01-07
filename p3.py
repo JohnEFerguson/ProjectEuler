89c6a3cd5d61a5fc76561616c422459d057bb3aa
Jack Ferguson <jef1@williams.edu>
2018-01-07 14:51:17 -0500

    p3

---
+++ p3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#
+# Jack Ferguson 2017
+#
+# Problem 3
+#
+# q: Find the largest prime factor of 600851475143
+# a: 6857
+#
+
+from helpers.is_prime import is_prime
+
+NUM = 600851475143
+ans = 0
+n = NUM
+i = 2
+while i**2 < NUM:
+  if n%i == 0:
+    n /= i 
+    ans = i 
+  else: i += 1
+if n > ans: ans = n
+
+print "p3: " + str(ans)
+
