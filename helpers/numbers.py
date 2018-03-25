b47435c7ebe3c7691ec81cea73121e01ba2301a1
John (Jack) Ferguson <jef1@williams.edu>
2018-03-25 17:15:41 -0400

    Delete numbers.py

--- helpers/numbers.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-def number_string(n):
-    ones = ['', 'one', 'two', 'three', 'four','five', 'six', 'seven', 'eight', 'nine']
-    tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-    then = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-    hund = 'hundred'
