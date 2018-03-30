60f72e0dcc47598327b68b48b1b5ede6a0059d15
Jack Ferguson <jef1@williams.edu>
2018-03-30 13:59:41 -0400

    divisors

--- helpers/divisors.py
+++ helpers/divisors.py
@@ -18,4 +18,17 @@ def sum_divisors(n):
             if n/i != i: sum += n/i
     return sum + 1
 
+def get_divisors(n):
+    divs = set()
+    for i in range(1, int(sqrt(n)+1)):
+        if n%i == 0: 
+            divs.add(i)
+            divs.add(n/i)
+    return divs
+
+''' test code
+for i in range(1, 5000):
+    if len(get_divisors(i)) != count_divisors(i): print i 
+'''
+            
     
